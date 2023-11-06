cb6b39fb203b4785752a0fd3d09287f04d791e07
summaiya <summaiyasuhaib@4csolutions.in>
2023-11-06 20:14:46 +0530

    adding files

---
+++ command.py
@@ -0,0 +1,5 @@
+from sys import argv
+print("the number of line arguments",len(argv))
+print("line arguments",argv)
+for x in argv:
+print (x)
---
+++ type casting.py
@@ -0,0 +1,6 @@
+x = int(1)
+y = int(2.8)
+z = int("3")
+a=10
+
+print(bool(0.0))
